63990186c7e1e204e1ea7cf8082cd6f6b27b289f
Rashidat <rawsheedah@yahoo.com>
2024-04-12 13:54:30 +0100

    Amend errors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-const PORT = 5000
+const PORT = 3000
 app.listen(PORT, () => {
     console.log('server is running on port 3000');
 })
